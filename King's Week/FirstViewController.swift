a1ef6f885f2b430e30272c7fd07c367def8aebb3
Ishita Mediratta <ishitamediratta19@gmail.com>
2017-04-24 09:46:48 +0530

    Added table view

--- King's Week/FirstViewController.swift
+++ King's Week/FirstViewController.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var table: UITableView!
+    
+    let days = ["1", "2", "3", "4"]
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell"/*Identifier*/, for: indexPath)
+        cell.textLabel?.text = days[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return days.count
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
